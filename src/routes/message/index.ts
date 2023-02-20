0e56c1e81da44cdd05532581a320bcf1abdc362c
Yen-Lin <46831150+wuyenlin@users.noreply.github.com>
2023-02-20 10:38:33 +0800

    listen to webhook events and use its reply token to reply message (#2)
    * listen to webhook event and use its reply token to reply message

--- src/routes/message/index.ts
+++ src/routes/message/index.ts
@@ -1,20 +1,37 @@
 import Router from 'koa-router';
-import { singleton } from 'tsyringe';
+import { inject, singleton } from 'tsyringe';
+import { LineHandler } from '../../handler';
+import { ContainerType, State } from '../../types';
+import type { Logger } from 'winston';
 
 @singleton()
 export default class MessageRouter {
-  constructor(private readonly router: Router) {}
+  constructor(
+    private readonly router: Router<State>,
+    private readonly lineHandler: LineHandler,
+    @inject(ContainerType.LOGGER) private readonly logger: Logger,
+  ) {}
 
   public initialize() {
     this.router.get('/api/v1', async (context, next) => {
       context.body = 'message router';
+      this.logger.info('Message router');
       await next();
     });
 
     this.router.post('/api/v1/webhook', async (context, next) => {
-      //TODO: make request headers config
-      // https://developers.line.biz/en/docs/messaging-api/nodejs-sample/#routing-config
       context.body = 'message router webhook';
+      this.logger.info('Message router webhook');
+      const events = context.state.events;
+
+      for (const event of events) {
+        try {
+          await this.lineHandler.handleTextEvent(event);
+        } catch (e) {
+          //TODO: revisit
+          context.status = 500;
+        }
+      }
       await next();
     });
   }
--- src/types/webhook-options.ts
+++
@@ -1,7 +0,0 @@
-export type WebhookOptions = {
-  hostname: string;
-  path: string;
-  method: string;
-  headers: string;
-  body: string;
-};
