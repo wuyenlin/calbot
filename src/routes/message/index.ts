cf211db082945447d49a8a89b28b996fb38ee7d7
wuyenlin <finne0831@gmail.com>
2023-02-20 10:31:37 +0800

    added debug logging for deploy use

--- src/routes/message/index.ts
+++ src/routes/message/index.ts
@@ -1,22 +1,28 @@
 import Router from 'koa-router';
-import { singleton } from 'tsyringe';
+import { inject, singleton } from 'tsyringe';
 import { LineHandler } from '../../handler';
-import type { State } from '../../types';
+import { ContainerType, State } from '../../types';
+import type { Logger } from 'winston';
 
 @singleton()
 export default class MessageRouter {
-  constructor(private readonly router: Router<State>, private readonly lineHandler: LineHandler) {}
+  constructor(
+    private readonly router: Router<State>,
+    private readonly lineHandler: LineHandler,
+    @inject(ContainerType.LOGGER) private readonly logger: Logger,
+  ) {}
 
   public initialize() {
     this.router.get('/api/v1', async (context, next) => {
       context.body = 'message router';
+      this.logger.info('Message router');
       await next();
     });
 
     this.router.post('/api/v1/webhook', async (context, next) => {
       context.body = 'message router webhook';
+      this.logger.info('Message router webhook');
       const events = context.state.events;
-      console.log(events);
 
       for (const event of events) {
         try {
